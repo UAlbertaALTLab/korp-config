b4c4416178ab49e637ef3149be94c17e699e3165
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2024-10-11 15:58:01 -0600

    Base maskwacis-sentences corpus

--- docker/backend/import_vrt.sh
+++ docker/backend/import_vrt.sh
@@ -10,6 +10,9 @@ VRT_FORMAT_STRUCTURE="-P word -P lemma -P analysis -P deps -P gloss -S sentence:
 if [ "$NAME" = "bloomfield" ]; then
     VRT_FORMAT_STRUCTURE="-P word -P lemma -P analysis -P deps -P gloss -S sentence:0+id -S paragraph -S text:2+id+title+author -S corpus:0+id+lang -U \"\""
 fi
+if [ "$NAME" = "maskwacis-sentences" ]; then
+   VRT_FORMAT_STRUCTURE="-P word -P analysis -S sentence:0+id+translation+recording+transcription+semantic-class  -U \"\""
+fi
 echo "Use ./import_vrt.sh filename_without_extension"
 echo "for example, ./import_vrt.sh wolfart_ahenakew"
 echo "file must be in the $VRT_FILES folder"
